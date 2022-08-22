1460e879cbfb466f8084c23bbbba4483a4c1618e
jan.schroeder <jan.schroeder@dussmann.de>
2022-08-22 10:55:52 +0200

    accepted answer

--- Fizz_Buzz/fizzbuzz.py
+++ Fizz_Buzz/fizzbuzz.py
@@ -1,1 +1,22 @@
 """ testing module """
+
+from typing import List
+
+
+def fizzBuzz(n: int) -> List[str]:
+    answer = []
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            answer.append("FizzBuzz")
+        elif i % 3 == 0:
+            answer.append("Fizz")
+        elif i % 5 == 0:
+            answer.append("Buzz")
+        else:
+            answer.append(str(i))
+    return answer
+
+
+if __name__ == '__main__':
+    n = 15
+    print(fizzBuzz(n))
