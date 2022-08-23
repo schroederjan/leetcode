b7511e97366c081fc6e248b508986396e4f36686
jan.schroeder <jan.schroeder@dussmann.de>
2022-08-23 17:18:44 +0200

    added new version

--- Study_Plan/isomorphic_strings.py
+++ Study_Plan/isomorphic_strings.py
@@ -35,6 +35,12 @@ class Solution:
         # return False
 
 
+# one liner
+#  class Solution:
+#      def isIsomorphic(self, s: str, t: str) -> bool:
+#          return len(set(s)) == len(set(t)) == len(set(zip(s, t)))
+
+
 if __name__ == '__main__':
     sol = Solution()
 
