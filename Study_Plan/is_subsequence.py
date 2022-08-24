0693fc14ec370da4cb4c07808a5f4621cddc2142
schroederjan <schroeder_jan@posteo.de>
2022-08-24 17:43:51 +0200

    use iterator for check list

--- Study_Plan/is_subsequence.py
+++ Study_Plan/is_subsequence.py
@@ -1,21 +1,25 @@
-""" """
+""" Solution for Problem """
 
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        print(list(s))
-        print(list(t))
-
-        if s in list(t):
-            return True
-        else:
-            return False
+        t_iter = iter(t)
+        # loop through letters and check against each letter in t
+        for letter in s:
+            if letter not in t_iter:
+                return False
+        return True
 
 
 if __name__ == '__main__':
     solution = Solution()
 
     s = "abc"
+    t = "ahbgdc"
+
+    print(solution.isSubsequence(s, t))
+
+    s = "x"
     t = "ahbgdc"
 
     print(solution.isSubsequence(s, t))
