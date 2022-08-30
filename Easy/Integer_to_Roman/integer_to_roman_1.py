54e994b8dd06702ad24eefa75986e409e520506a
schroederjan <schroeder_jan@posteo.de>
2022-08-30 08:52:54 +0200

    first real solution

--- Easy/Integer_to_Roman/integer_to_roman_1.py
+++ Easy/Integer_to_Roman/integer_to_roman_1.py
@@ -1,40 +1,31 @@
 class Solution():
     def __init__(self,):
         self.roman_symbols_dict = {
-            'I': 1,
-            'V': 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
+            1000: 'M',  900: 'CM',
+            500: 'D', 400: 'CD',
+            100: 'C', 90: 'XC',
+            50: 'L', 40: 'XL',
+            10: 'X', 9: 'IX',
+            5: 'V', 4: 'IV', 1: 'I',
         }
 
     def intToRoman(self, num: int) -> str:
-
-        return 'NONE'
+        """
+        run through each entry in test_dict
+        check if num is dividable by current key in dict
+        if it is it returns a 1 and adds the current val to a string
+        then the mathed key is substractred from the current num
+        the same continues until gone through the whole dict
+        """
+        res = ""
+        for key, val in self.roman_symbols_dict.items():
+            res += val * (num // key)
+            num -= key * (num // key)
+        print(res)
+        return res
 
 
 if __name__ == '__main__':
 
-    solution = Solution()
-
-    test_dict = {
-    }
-
-    def _check_solution(solution, should):
-        if solution == should:
-            result = True
-        else:
-            result = False
-        return result
-
-    # for index, s in enumerate(test_list):
-    for index, num in enumerate(test_dict.keys()):
-        print("\n")
-        print(f"case: {index}")
-        print(f"input: {num}")
-        result = solution.intToRoman(num)
-        print(f"solution: {result}")
-        print(f"should: {test_dict[num]}")
-        print(f"check: {_check_solution(result, test_dict[num])}")
+    sol = Solution()
+    sol.intToRoman(199)
